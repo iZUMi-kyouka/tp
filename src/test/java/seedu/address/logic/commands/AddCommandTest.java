4157597d68a869e6f06e17b22c2f693d893250ee
Hoo Di Heng <Hoodiheng@gmail.com>
2025-10-09 00:13:07 +0800

    Fix CI/CD

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -50,7 +50,7 @@ public class AddCommandTest {
         AddCommand addCommand = new AddCommand(validRecruit);
         ModelStub modelStub = new ModelStubWithRecruit(validRecruit);
 
-        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_PERSON, () -> addCommand.execute(modelStub));
+        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_RECRUIT, () -> addCommand.execute(modelStub));
     }
 
     @Test
