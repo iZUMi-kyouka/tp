466f87e89d5469b0f3ff05428cf76b8bda16f0f7
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-10-16 17:12:59 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -8,8 +8,8 @@ import static seedu.address.logic.parser.CliSyntax.SEARCH_PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.SEARCH_PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.SEARCH_PREFIX_TAG;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
