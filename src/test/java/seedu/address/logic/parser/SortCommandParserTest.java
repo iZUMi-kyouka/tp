57c3758fe8e8b822c724b8e6bc3839553d3cddee
hylinny <hylin899@gmail.com>
2025-10-23 04:36:04 +0800

    Add test cases

---
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -0,0 +1,197 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.SORT_PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.SORT_PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.SORT_PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.SORT_PREFIX_PHONE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.commands.SortCommand.SortCriterion;
+import seedu.address.logic.commands.SortCommand.SortOrder;
+
+/**
+ * Contains tests for SortCommandParser.
+ */
+public class SortCommandParserTest {
+
+    private SortCommandParser parser = new SortCommandParser();
+
+    @Test
+    public void parse_validSingleFieldAscending_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+        assertParseSuccess(parser, "-n asc", expectedCommand);
+    }
+
+    @Test
+    public void parse_validSingleFieldDescending_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_PHONE, SortOrder.DESCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+        assertParseSuccess(parser, "-p desc", expectedCommand);
+    }
+
+    @Test
+    public void parse_validMultipleFields_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+        criteria.add(new SortCriterion(SORT_PREFIX_PHONE, SortOrder.DESCENDING));
+        criteria.add(new SortCriterion(SORT_PREFIX_ADDRESS, SortOrder.ASCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+        assertParseSuccess(parser, "-n asc -p desc -a asc", expectedCommand);
+    }
+
+    @Test
+    public void parse_validAllFields_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+        criteria.add(new SortCriterion(SORT_PREFIX_PHONE, SortOrder.DESCENDING));
+        criteria.add(new SortCriterion(SORT_PREFIX_EMAIL, SortOrder.ASCENDING));
+        criteria.add(new SortCriterion(SORT_PREFIX_ADDRESS, SortOrder.DESCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+        assertParseSuccess(parser, "-n asc -p desc -e asc -a desc", expectedCommand);
+    }
+
+    @Test
+    public void parse_caseInsensitiveOrder_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+
+        assertParseSuccess(parser, "-n ASC", expectedCommand);
+        assertParseSuccess(parser, "-n AsC", expectedCommand);
+        assertParseSuccess(parser, "-n aSc", expectedCommand);
+    }
+
+    @Test
+    public void parse_orderPreservation_success() {
+        // Test that left-to-right order is preserved
+        List<SortCriterion> criteria1 = new ArrayList<>();
+        criteria1.add(new SortCriterion(SORT_PREFIX_PHONE, SortOrder.DESCENDING));
+        criteria1.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+
+        SortCommand expectedCommand1 = new SortCommand(criteria1);
+        assertParseSuccess(parser, "-p desc -n asc", expectedCommand1);
+
+        List<SortCriterion> criteria2 = new ArrayList<>();
+        criteria2.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+        criteria2.add(new SortCriterion(SORT_PREFIX_PHONE, SortOrder.DESCENDING));
+
+        SortCommand expectedCommand2 = new SortCommand(criteria2);
+        assertParseSuccess(parser, "-n asc -p desc", expectedCommand2);
+    }
+
+    @Test
+    public void parse_extraSpaces_success() {
+        List<SortCriterion> criteria = new ArrayList<>();
+        criteria.add(new SortCriterion(SORT_PREFIX_NAME, SortOrder.ASCENDING));
+
+        SortCommand expectedCommand = new SortCommand(criteria);
+
+        assertParseSuccess(parser, "-n  asc", expectedCommand);
+        assertParseSuccess(parser, "-n   asc", expectedCommand);
+        assertParseSuccess(parser, "  -n asc  ", expectedCommand);
+    }
+
+    @Test
+    public void parse_caseInsensitivePrefix_failure() {
+        assertParseFailure(parser, "-N asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArgs_failure() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_whitespaceOnly_failure() {
+        assertParseFailure(parser, "   ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPrefix_failure() {
+        assertParseFailure(parser, "-x asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-x asc -y desc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "n asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "p desc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "n/asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidOrder_failure() {
+        assertParseFailure(parser, "-n invalid",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-n ascending",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-p up",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingOrder_failure() {
+        assertParseFailure(parser, "-n",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-n -p",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingPrefix_failure() {
+        assertParseFailure(parser, "asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "desc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_preamblePresent_failure() {
+        assertParseFailure(parser, "some text -n asc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "hello -p desc",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_duplicatePrefix_failure() {
+        assertParseFailure(parser,
+                "-n asc -n desc",
+                Messages.getErrorMessageForDuplicatePrefixes(SORT_PREFIX_NAME));
+
+        assertParseFailure(parser,
+                "-p asc -p desc",
+                Messages.getErrorMessageForDuplicatePrefixes(SORT_PREFIX_PHONE));
+    }
+}
+
