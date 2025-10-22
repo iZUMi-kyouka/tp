97511735308adde21257c5717044a7fe4825f0f6
hylinny <hylin899@gmail.com>
2025-10-23 04:33:42 +0800

    Define sorting prefix and order fields

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -19,4 +19,9 @@ public class CliSyntax {
     public static final Prefix SEARCH_PREFIX_EMAIL = new Prefix("-e");
     public static final Prefix SEARCH_PREFIX_ADDRESS = new Prefix("-a");
     public static final Prefix SEARCH_PREFIX_TAG = new Prefix("-t");
+
+    public static final Prefix SORT_PREFIX_NAME = new Prefix("-n");
+    public static final Prefix SORT_PREFIX_PHONE = new Prefix("-p");
+    public static final Prefix SORT_PREFIX_EMAIL = new Prefix("-e");
+    public static final Prefix SORT_PREFIX_ADDRESS = new Prefix("-a");
 }
--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -42,6 +42,8 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_SORT_ASCENDING = "asc";
+    public static final String VALID_SORT_DESCENDING = "desc";
 
     public static final String ID_DESC_AMY = " " + PREFIX_ID + VALID_ID_AMY;
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
