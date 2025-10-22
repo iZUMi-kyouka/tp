ab092614c54edb51c74b2f3e5b2184f362af350e
Eilyss <foo.fang.han@gmail.com>
2025-10-23 00:57:27 +0800

    Add support for numeric characters in Name

--- src/main/java/seedu/address/model/recruit/Name.java
+++ src/main/java/seedu/address/model/recruit/Name.java
@@ -16,7 +16,7 @@ public class Name implements Comparable<Name> {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{L}][\\p{L} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{L}\\d][\\p{L}\\d ]*";
 
     public final String fullName;
 
