e0e51606bd115bccbe1692d8cefbd3bc6f6a1903
Hoo Di Heng <Hoodiheng@gmail.com>
2025-10-23 22:03:24 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.recruit.Recruit;
