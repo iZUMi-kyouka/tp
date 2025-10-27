8a1b4a99f2b9ba294c067cb5330997ee8c50ed90
Florian Viorel Tanely <izkx73@gmail.com>
2025-10-28 02:28:15 +0800

    Fix more failing tests

--- src/test/java/seedu/address/commons/util/IntervalTest.java
+++ src/test/java/seedu/address/commons/util/IntervalTest.java
@@ -6,11 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class IntervalTest {
-    
+
     @Test
     public void constructor() {
         // Integer interval
         Interval<Integer> i1 = new Interval<>(-10, 10);
+
+        // Double interval
+        Interval<Double> i2 = new Interval<>(-10.99, 9.99);
+
+        // Char interval
+        Interval<Character> i3 = new Interval<>('a', 'z');
+    }
+
+    @Test
+    public void contains() {
+        // Integer interval
+        Interval<Integer> i1 = new Interval<>(-10, 10);
+        assertTrue(i1 instanceof Interval);
         assertTrue(i1.contains(0));
         assertTrue(i1.contains(10));
         assertTrue(i1.contains(-10));
@@ -19,6 +32,7 @@ public class IntervalTest {
 
         // Double interval
         Interval<Double> i2 = new Interval<>(-10.99, 9.99);
+        assertTrue(i1 instanceof Interval);
         assertTrue(i2.contains(0.001));
         assertTrue(i2.contains(-10.99));
         assertTrue(i2.contains(9.99));
