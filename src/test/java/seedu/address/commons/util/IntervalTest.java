f27fa333babf1ed69b435f0b120f3ee7104031e5
Florian Viorel Tanely <izkx73@gmail.com>
2025-10-28 02:25:32 +0800

    Add junit tests for Interval class

---
+++ src/test/java/seedu/address/commons/util/IntervalTest.java
@@ -0,0 +1,27 @@
+package seedu.address.commons.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IntervalTest {
+    
+    @Test
+    public void constructor() {
+        // Integer interval
+        Interval<Integer> i1 = new Interval<>(-10, 10);
+        assertTrue(i1.contains(0));
+        assertTrue(i1.contains(10));
+        assertTrue(i1.contains(-10));
+        assertFalse(i1.contains(-11));
+        assertFalse(i1.contains(11));
+
+        // Double interval
+        Interval<Double> i2 = new Interval<>(-10.99, 9.99);
+        assertTrue(i2.contains(0.001));
+        assertTrue(i2.contains(-10.99));
+        assertTrue(i2.contains(9.99));
+        assertFalse(i2.contains(10.99));
+    }
+}
