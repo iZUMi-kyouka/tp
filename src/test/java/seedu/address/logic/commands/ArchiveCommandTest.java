44dad10226b4bc08a010c21524ab629d9d77054c
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-10-29 16:02:33 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.ParserUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
