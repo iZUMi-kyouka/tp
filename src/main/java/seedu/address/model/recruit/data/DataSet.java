1eaa583e8817470c471b6f72b83008884d0debc2
Florian Viorel Tanely <izkx73@gmail.com>
2025-10-30 00:02:54 +0800

    Implement DataSet class

---
+++ src/main/java/seedu/address/model/recruit/data/DataSet.java
@@ -0,0 +1,50 @@
+package seedu.address.model.recruit.data;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.TreeSet;
+
+/** 
+ * A minimally extended TreeSet with a primary data attribute.
+ * Users are responsible for ensuring the current state of primary data
+ * is valid i.e. it should generally be an item that is equal to one of the
+ * items contained in this TreeSet.
+ */
+public class DataSet<T extends Data> extends TreeSet<T> {
+    private Optional<T> primaryData;
+
+    /**
+     * Constructs a DataSet containing items in {@code ts}. The first item
+     * in the resulting DataSet, will be set as the primary data.
+     * @param ts
+     */
+    public DataSet(Collection<? extends T> ts) {
+        super(ts);
+        primaryData = this.isEmpty() ? Optional.empty() : Optional.of(this.first());
+    }
+
+    /**
+     * Constructs a DataSet containing items in {@code ts}, where the ordering
+     * of the items is maintained based on the given {@code comparator}.
+     * The first item in the resulting DataSet, will be set as the primary data.
+     * @param ts
+     */
+    public DataSet(Collection<? extends T> ts, Comparator<? super T> comp) {
+        super(comp);
+        this.addAll(ts);
+    }
+
+    public boolean setPrimary(T data) {
+        if (this.stream().filter(t -> t.equals(data)).count() == 0) {
+            return false;
+        }
+
+        primaryData = Optional.of(data);
+        return true;
+    }
+
+    public Optional<T> getPrimary() {
+        return primaryData;
+    }
+}
